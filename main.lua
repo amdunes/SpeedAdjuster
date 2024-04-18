baad4441a3859a337b4e2487d9b86cf0fb1c7d5c
amdunes <161895636+amdunes@users.noreply.github.com>
2024-04-18 08:47:30 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 local default_speed = 10
 local current_speed = default_speed
+local increase_key = "Numpad +"
+local decrease_key = "Numpad -"
+local reset_key = "Numpad *"
 
 function set_player_speed(speed)
     local player = get_client_player()
@@ -22,7 +25,32 @@ function decrease_speed()
     set_player_speed(current_speed)
 end
 
+function draw_speed_tab()
+    if ImGui.BeginTabItem("Speed Settings") then
+        ImGui.Text("Increase Speed Keybind:")
+        _, increase_key = ImGui.InputText("##increase_key", increase_key, 20)
+
+        ImGui.Text("Decrease Speed Keybind:")
+        _, decrease_key = ImGui.InputText("##decrease_key", decrease_key, 20)
+
+        ImGui.Text("Reset Speed Keybind:")
+        _, reset_key = ImGui.InputText("##reset_key", reset_key, 20)
+
+        ImGui.EndTabItem()
+    end
+end
+
 -- Keybinds
-input.register_hotkey("increase_speed", "Increase Player Speed", increase_speed, "Numpad +")
-input.register_hotkey("decrease_speed", "Decrease Player Speed", decrease_speed, "Numpad -")
-input.register_hotkey("reset_speed", "Reset Player Speed", reset_player_speed, "Numpad *")
+input.register_hotkey("increase_speed", "Increase Player Speed", increase_speed, increase_key)
+input.register_hotkey("decrease_speed", "Decrease Player Speed", decrease_speed, decrease_key)
+input.register_hotkey("reset_speed", "Reset Player Speed", reset_player_speed, reset_key)
+
+function draw()
+    if ImGui.Begin("Speed Mod") then
+        if ImGui.BeginTabBar("SpeedModTabs") then
+            draw_speed_tab()
+            ImGui.EndTabBar()
+        end
+        ImGui.End()
+    end
+end
